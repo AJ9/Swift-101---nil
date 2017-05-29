43895a0a93e9195af51a66fb506a17e24b695580
Adam Gask <ajgask@gmail.com>
2017-05-29 17:09:43 +0100

    Update Contents.swift

--- Swift 101 - nil.playground/Contents.swift
+++ Swift 101 - nil.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*:
  # Swift 101 - nil
- http://aj9.co.uk/swift-101-nil
+ https://medium.com/pretty-swifty/swift-101-nil-423813b56dfb
  
  A super quick blog to go over a key keyword in Swift - `nil`.
  */
